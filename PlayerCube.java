aa38e1c07e7723ba4eea71eee581c0e41ee72b00
Molnár András Imre <m.andras.imre00@gmail.com>
2020-05-03 12:11:05 +0200

    Player cube representation.

---
+++ PlayerCube.java
@@ -0,0 +1,14 @@
+
+public class PlayerCube {
+
+    public static int player_face, player_leftside, player_upside, player_rightside, player_downside, player_under;
+
+    PlayerCube() {
+        this.player_face = 0;
+        this.player_leftside = 1;
+        this.player_downside = 2;
+        this.player_rightside = 3;
+        this.player_upside = 4;
+        this.player_under = 5;
+    }
+}
